18c0acf99cd39c78a6d86a72299f8bae3f59dd3a
장재영 <jangjy0105@gmail.com>
2023-05-23 17:38:21 +0900

    mysql연결

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express')
 const app = express()
 const port = 5000
@@ -8,10 +9,29 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
 })
 
+connection.connect();
+
 app.get('/api/hello', async(req, res) => {
-  res.send('hello');
+  connection.query(
+    "SELECT * FROM MOVIE",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
